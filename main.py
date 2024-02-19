a31e0d0a1f751ce2919366a64e05023eea5f8ec4
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-02-19 11:39:31 +0300

    Installed Click, Added cli.py and the respective commands for interaction with the database

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from lib.models import Base
-from lib.models import Recipe, Category, Author
+from lib.models import Base, Recipe, Author, Category
 
+engine = create_engine('sqlite:///recipes.db')
 
+Base.metadata.create_all(engine)
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///recipes.db')
-    Base.metadata.create_all(engine)
-    
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    import ipdb; ipdb.set_trace()
+Session = sessionmaker(bind=engine)
+session = Session()
